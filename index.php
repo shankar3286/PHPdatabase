3d0e6ee1271acffa9452d87009fb5475469d27ce
Shankargoudar <Skarigoudar87@gmail.com>
2019-01-15 22:23:14 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE Html>
+<html>
+<body>
+<title>my first page </title>
+
+<form action='C:\xampp1\htdocs\Test\submit.php' method='post'>
+	Firstname:<br>
+	<Input type='text' name='Firstname'>
+	<br>
+	Lastname:<br>
+	<input type='text' name='Lastname'>
+	<br><br>
+	<input	type='Submit'	Value='submit'>
+	</form>
+</body>
+</html>
+	
